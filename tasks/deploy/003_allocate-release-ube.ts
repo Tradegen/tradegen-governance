a9ed8e5aef4ced8562876ed254727c5033f6f2ae
Ian Macalinao <github@igm.pub>
2021-04-22 11:15:48 -0500

    transfer release UBE to operator

--- tasks/deploy/003_allocate-release-ube.ts
+++ tasks/deploy/003_allocate-release-ube.ts
@@ -2,31 +2,33 @@ import { DeployerFn, doTx } from "@ubeswap/hardhat-celo";
 import { getAddress, parseEther } from "ethers/lib/utils";
 import { DeployersMap } from ".";
 import { ReleaseUbe__factory } from "../../build/types/";
-import initialHolders from "../../genesis-ube.json";
+import { OPERATOR } from "./config";
+import { GENESIS_UBE } from "./genesis-ube";
 
-const addresses = initialHolders.map((a) => getAddress(a[0]!));
-const amounts = initialHolders.map((a) =>
-  parseEther(a[1]?.split(",").join("")!)
-);
+const addresses = GENESIS_UBE.map((a) => getAddress(a[0]!));
+// const amounts = GENESIS_UBE.map((a) => parseEther(a[1]?.split(",").join("")!));
+const amounts = GENESIS_UBE.map((a) => parseEther("0.1"));
 
 export const allocateReleaseUbe: DeployerFn<{}> = async ({
   deployer,
   getAddresses,
 }) => {
-  if ((initialHolders as readonly unknown[]).length === 0) {
+  if (GENESIS_UBE.length === 0) {
     throw new Error("No initial holders of ReleaseUbe specified.");
   }
-  const { TimelockExecutive, ReleaseUbe } = getAddresses<
-    DeployersMap,
-    "token" | "timelocks"
-  >("token", "timelocks");
+  const { ReleaseUbe } = getAddresses<DeployersMap, "token">("token");
   const releaseUbe = ReleaseUbe__factory.connect(ReleaseUbe, deployer);
 
-  await doTx("Allocate ReleaseUbe", releaseUbe.allocate(addresses, amounts));
+  console.log("Allocating to", JSON.stringify(addresses));
 
   await doTx(
-    "Change owner of ReleaseUbe to the Executive timelock",
-    releaseUbe.transferOwnership(TimelockExecutive)
+    "Allocate ReleaseUbe",
+    releaseUbe.allocate(addresses, amounts, { gasLimit: 8000000 })
+  );
+
+  await doTx(
+    "Change owner of ReleaseUbe to the Operator",
+    releaseUbe.transferOwnership(OPERATOR)
   );
   return {};
 };
