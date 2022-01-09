8c20811a7894690c842437d8eb7a079e85e5f234
Xavier Negron <xaviernegron888@gmail.com>
2022-01-09 15:48:08 -0800

    Deploy Timelock contract to mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
     // only: [],
     // except: []
   },
-  defaultNetwork: "alfajores",
+  defaultNetwork: "mainnet",
   gasReporter: {
     enabled: process.env.REPORT_GAS ? true : false,
     currency: "USD",
